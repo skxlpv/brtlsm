73a64b7df832ec727208a081d216f900c454c6f4
skxlpv <olexandertym0000@gmail.com>
2025-05-20 12:48:44 +0300

    feat: header; example nav list

--- client/src/features/ui/Header.jsx
+++ client/src/features/ui/Header.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
+import header from "../../assets/images/ui/header-clouds.jpg"
 
 const Header = () => {
     return (
-        <div className="w-full px-10 py-6">
-            <div className="mb-20">
-                <h1 className="mb-8">Internal Bleeding</h1>
-                <ul className="cursor-pointer w-fit flex flex-col gap-3">
-                    <li className="button-hover">History</li>
-                    <li className="button-hover">Gallery</li>
-                    <li className="button-hover">LinkTree</li>
+    <>
+        <div className="w-full px-20 py-4">
+            <div className="w-full px-10 flex flex-row items-center">
+                <h1 className="absolute top-10">under</h1>
+                <ul className="w-fit flex flex-col gap-2 relative top-10 left-60">
+                    {Object.entries(navigationItems).map(([key, value], index) => (
+                        <li
+                            key={key}
+                            className={`button-hover relative text-md font-bold w-fit cursor-pointer`}
+                            style={{ right: `${index % 2 === 0? index*2: -index*2}rem`, top: `3rem` }}
+                        >
+                            {value}
+                        </li>
+                    ))}
                 </ul>
             </div>
         </div>
-    );
+        <hr className="border-red-900 absolute top-16 w-full" />
+    </>
+);
 };
+
+const navigationItems = {
+    events: "усі заходи",
+    add_event: "додати подію",
+    search: "шукати",
+}
 
 export default Header;
